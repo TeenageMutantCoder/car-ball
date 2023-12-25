6bb612454fa039c13d1e6f69b8cb5db5014510cf
Stevon Wright <stevonisepic@gmail.com>
2023-12-25 18:06:47 -0500

    Refactor, added ground

--- src/main.ts
+++ src/main.ts
@@ -8,30 +8,44 @@ import {
   HemisphericLight,
   type Mesh,
   MeshBuilder,
+  StandardMaterial,
+  Color3,
 } from "@babylonjs/core";
 
 class App {
+  readonly #canvas: HTMLCanvasElement;
+  readonly #engine: Engine;
+  readonly #scene: Scene;
+
   constructor() {
-    // create the canvas html element and attach it to the webpage
+    this.#canvas = this.#createCanvas();
+    this.#engine = new Engine(this.#canvas, true);
+    this.#scene = this.#createScene();
+
+    this.#addInspectorListener();
+  }
+
+  #createCanvas(): HTMLCanvasElement {
     const canvas = document.createElement("canvas");
     canvas.style.width = "100%";
     canvas.style.height = "100%";
     canvas.id = "gameCanvas";
     document.body.appendChild(canvas);
+    return canvas;
+  }
 
-    // initialize babylon scene and engine
-    const engine = new Engine(canvas, true);
-    const scene = new Scene(engine);
+  #createScene(): Scene {
+    const scene = new Scene(this.#engine);
 
     const camera: ArcRotateCamera = new ArcRotateCamera(
       "Camera",
       Math.PI / 2,
-      Math.PI / 2,
-      2,
+      (3 * Math.PI) / 8,
+      50,
       Vector3.Zero(),
       scene,
     );
-    camera.attachControl(canvas, true);
+    camera.attachControl(this.#canvas, true);
 
     const light1: HemisphericLight = new HemisphericLight(
       "light1",
@@ -41,15 +55,26 @@ class App {
 
     light1.intensity = 0.7;
 
-    const sphere: Mesh = MeshBuilder.CreateSphere(
-      "sphere",
-      { diameter: 1 },
+    const boxSize = 5;
+    const box: Mesh = MeshBuilder.CreateBox("box", { size: boxSize }, scene);
+
+    box.position.y = 0.5 * boxSize;
+
+    const ground = MeshBuilder.CreateGround(
+      "ground",
+      { width: 100, height: 100 },
       scene,
     );
+    ground.position.y = 0;
 
-    sphere.position.y = 0;
+    const groundMat = new StandardMaterial("groundMat");
+    groundMat.diffuseColor = new Color3(0, 1, 0);
+    ground.material = groundMat;
 
-    // hide/show the Inspector
+    return scene;
+  }
+
+  #addInspectorListener(): void {
     window.addEventListener("keydown", (ev) => {
       // Shift+Ctrl+Alt+I
       if (
@@ -58,18 +83,25 @@ class App {
         ev.altKey &&
         ev.key.toLowerCase() === "i"
       ) {
-        if (scene.debugLayer.isVisible()) {
-          scene.debugLayer.hide();
+        if (this.#scene.debugLayer.isVisible()) {
+          this.#scene.debugLayer.hide();
         } else {
-          void scene.debugLayer.show();
+          void this.#scene.debugLayer.show();
         }
       }
     });
+  }
 
-    // run the main render loop
-    engine.runRenderLoop(() => {
-      scene.render();
+  run(): void {
+    this.#engine.runRenderLoop(() => {
+      this.#scene.render();
     });
   }
 }
-new App();
+
+const startApp = (): void => {
+  const app = new App();
+  app.run();
+};
+
+startApp();
