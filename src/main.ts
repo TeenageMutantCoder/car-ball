718144104184b45ae422e6bd83274e4267aecc8b
Stevon Wright <stevonisepic@gmail.com>
2023-12-25 16:32:08 -0500

    Add babylonjs with starter code

---
+++ src/main.ts
@@ -0,0 +1,75 @@
+import "@babylonjs/core/Debug/debugLayer";
+import "@babylonjs/inspector";
+import {
+  Engine,
+  Scene,
+  ArcRotateCamera,
+  Vector3,
+  HemisphericLight,
+  type Mesh,
+  MeshBuilder,
+} from "@babylonjs/core";
+
+class App {
+  constructor() {
+    // create the canvas html element and attach it to the webpage
+    const canvas = document.createElement("canvas");
+    canvas.style.width = "100%";
+    canvas.style.height = "100%";
+    canvas.id = "gameCanvas";
+    document.body.appendChild(canvas);
+
+    // initialize babylon scene and engine
+    const engine = new Engine(canvas, true);
+    const scene = new Scene(engine);
+
+    const camera: ArcRotateCamera = new ArcRotateCamera(
+      "Camera",
+      Math.PI / 2,
+      Math.PI / 2,
+      2,
+      Vector3.Zero(),
+      scene,
+    );
+    camera.attachControl(canvas, true);
+
+    const light1: HemisphericLight = new HemisphericLight(
+      "light1",
+      new Vector3(1, 1, 0),
+      scene,
+    );
+
+    light1.intensity = 0.7;
+
+    const sphere: Mesh = MeshBuilder.CreateSphere(
+      "sphere",
+      { diameter: 1 },
+      scene,
+    );
+
+    sphere.position.y = 0;
+
+    // hide/show the Inspector
+    window.addEventListener("keydown", (ev) => {
+      // Shift+Ctrl+Alt+I
+      if (
+        ev.shiftKey &&
+        ev.ctrlKey &&
+        ev.altKey &&
+        ev.key.toLowerCase() === "i"
+      ) {
+        if (scene.debugLayer.isVisible()) {
+          scene.debugLayer.hide();
+        } else {
+          void scene.debugLayer.show();
+        }
+      }
+    });
+
+    // run the main render loop
+    engine.runRenderLoop(() => {
+      scene.render();
+    });
+  }
+}
+new App();
