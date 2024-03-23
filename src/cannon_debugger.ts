4ff473d9fac0b8b49a16342cbd02b79e6413a29d
Stevon Wright <stevonisepic@gmail.com>
2024-03-23 03:26:45 -0400

    Install/move dependencies, remove havok physics, start setting up cannon physics and debugger

---
+++ src/cannon_debugger.ts
@@ -0,0 +1,268 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+/* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+
+// Mostly copied from https://github.com/neu5/cannon-es-debugger-babylonjs
+
+import {
+  Quaternion as CannonQuaternion,
+  Vec3 as CannonVector3,
+  Shape,
+} from "cannon-es";
+import type { FloatArray, Scene } from "@babylonjs/core";
+import {
+  Color3,
+  Mesh,
+  MeshBuilder,
+  Quaternion,
+  StandardMaterial,
+  Vector3,
+  VertexBuffer,
+  VertexData,
+} from "@babylonjs/core";
+
+import type {
+  Box,
+  Body as CannonBody,
+  Cylinder,
+  Sphere,
+  Trimesh,
+  World,
+} from "cannon-es";
+
+export interface DebugOptions {
+  color?: { r: number; g: number; b: number };
+  scale?: number;
+  onInit?: (body: CannonBody, mesh: Mesh, shape: Shape) => void;
+  onUpdate?: (body: CannonBody, mesh: Mesh, shape: Shape) => void;
+}
+
+const BABYLON_DEBUGGER: string = "CANNONES_DEBUGGER_BABYLONJS";
+const getMeshName = (name: string, num: number) =>
+  `${name}_${BABYLON_DEBUGGER}_${num}`;
+
+type CannonDebugger = new (
+  scene: Scene,
+  world: World,
+  debugOptions?: DebugOptions,
+) => { update: () => void };
+
+const cannonDebugger: CannonDebugger = function(
+  scene: Scene,
+  world: World,
+  {
+    color = { r: 0, g: 1, b: 0 },
+    scale = 1,
+    onInit,
+    onUpdate,
+  }: DebugOptions = {},
+) {
+  const meshes: Mesh[] = [];
+  const material = new StandardMaterial("myMaterial", scene);
+  material.diffuseColor = new Color3(color.r, color.g, color.b);
+  material.wireframe = true;
+  const tempVec0 = new CannonVector3();
+  const tempQuat0 = new CannonQuaternion();
+  let meshCounter: number = 0;
+
+  function createMesh(shape: Shape): Mesh | undefined {
+    let mesh;
+    const { SPHERE, BOX, PLANE, CYLINDER, TRIMESH } = Shape.types;
+
+    switch (shape.type) {
+      case BOX: {
+        mesh = MeshBuilder.CreateBox(
+          getMeshName("box", meshCounter),
+          {},
+          scene,
+        );
+        meshCounter = meshCounter + 1;
+        mesh.rotationQuaternion = mesh.rotationQuaternion || new Quaternion();
+        break;
+      }
+      case CYLINDER: {
+        const { height, numSegments, radiusBottom, radiusTop } =
+          shape as Cylinder;
+
+        mesh = MeshBuilder.CreateCylinder(
+          getMeshName("cylinder", meshCounter),
+          {
+            diameterTop: radiusTop * 2,
+            diameterBottom: radiusBottom * 2,
+            height,
+            tessellation: numSegments,
+          },
+          scene,
+        );
+        meshCounter = meshCounter + 1;
+        mesh.rotationQuaternion = mesh.rotationQuaternion || new Quaternion();
+        break;
+      }
+      case PLANE: {
+        mesh = MeshBuilder.CreatePlane(
+          getMeshName("plane", meshCounter),
+          { width: 10, height: 10 },
+          scene,
+        );
+        meshCounter = meshCounter + 1;
+        mesh.rotation = new Vector3(Math.PI / 2, 0, 0);
+        break;
+      }
+      case SPHERE: {
+        mesh = MeshBuilder.CreateSphere(
+          getMeshName("sphere", meshCounter),
+          { segments: 16 },
+          scene,
+        );
+        meshCounter = meshCounter + 1;
+        mesh.rotationQuaternion = mesh.rotationQuaternion || new Quaternion();
+        break;
+      }
+      case TRIMESH: {
+        mesh = new Mesh(getMeshName("trimesh", meshCounter), scene);
+
+        const vertexData = new VertexData();
+        const { indices, vertices } = shape as Trimesh;
+        const normals: FloatArray = [];
+
+        vertexData.positions = vertices;
+        vertexData.indices = indices as unknown as Uint16Array;
+        VertexData.ComputeNormals(vertices, mesh.getIndices(), normals, {
+          useRightHandedSystem: true,
+        });
+        mesh.setVerticesData(VertexBuffer.NormalKind, normals);
+        vertexData.applyToMesh(mesh);
+        meshCounter = meshCounter + 1;
+        mesh.rotationQuaternion = mesh.rotationQuaternion || new Quaternion();
+
+        break;
+      }
+    }
+
+    if (!mesh) {
+      return;
+    }
+
+    mesh.material = material;
+    scene.addMesh(mesh);
+
+    return mesh;
+  }
+
+  function scaleMesh(mesh: Mesh, shape: Shape): void {
+    const { SPHERE, BOX, PLANE, CYLINDER, TRIMESH } = Shape.types;
+
+    switch (shape.type) {
+      case BOX: {
+        const { halfExtents } = shape as Box;
+        const size = new Vector3(
+          halfExtents.x * scale * 2,
+          halfExtents.y * scale * 2,
+          halfExtents.z * scale * 2,
+        );
+        mesh.scaling = size;
+        break;
+      }
+      case CYLINDER: {
+        break;
+      }
+      case PLANE: {
+        break;
+      }
+      case SPHERE: {
+        const { radius } = shape as Sphere;
+        mesh.scalingDeterminant = radius * scale * 2;
+        break;
+      }
+      case TRIMESH:
+        mesh.scaling.x = scale;
+        mesh.scaling.y = scale;
+        mesh.scaling.z = scale;
+        break;
+    }
+  }
+
+  function typeMatch(mesh: Mesh, shape: Shape): boolean {
+    if (!mesh) {
+      return false;
+    }
+
+    return (
+      shape.type === Shape.types.SPHERE ||
+      shape.type === Shape.types.PLANE ||
+      shape.type === Shape.types.BOX ||
+      shape.type === Shape.types.CYLINDER ||
+      shape.type === Shape.types.TRIMESH
+    );
+  }
+
+  function updateMesh(index: number, shape: Shape): boolean {
+    let mesh = meshes[index];
+    let didCreateNewMesh = false;
+    if (!typeMatch(mesh, shape)) {
+      if (mesh) {
+        scene.removeMesh(mesh);
+      }
+
+      const newMesh = createMesh(shape);
+      if (newMesh) {
+        meshes[index] = mesh = newMesh;
+      }
+
+      didCreateNewMesh = true;
+    }
+    scaleMesh(mesh, shape);
+    return didCreateNewMesh;
+  }
+
+  function update(): void {
+    const meshesCopy = meshes;
+    const shapeWorldPosition = tempVec0;
+    const shapeWorldQuaternion = tempQuat0;
+    let meshIndex = 0;
+    for (const body of world.bodies) {
+      for (let i = 0; i !== body.shapes.length; i++) {
+        const shape = body.shapes[i];
+        const didCreateNewMesh = updateMesh(meshIndex, shape);
+        const mesh = meshesCopy[meshIndex];
+        if (mesh) {
+          // Get world position
+          body.quaternion.vmult(body.shapeOffsets[i], shapeWorldPosition);
+          body.position.vadd(shapeWorldPosition, shapeWorldPosition);
+          // Get world quaternion
+          body.quaternion.mult(body.shapeOrientations[i], shapeWorldQuaternion);
+          // Copy to meshes
+          mesh.position.set(
+            shapeWorldPosition.x,
+            shapeWorldPosition.y,
+            shapeWorldPosition.z,
+          );
+
+          if (mesh.rotationQuaternion) {
+            mesh.rotationQuaternion.set(
+              shapeWorldQuaternion.x,
+              shapeWorldQuaternion.y,
+              shapeWorldQuaternion.z,
+              shapeWorldQuaternion.w,
+            );
+          }
+
+          if (didCreateNewMesh && onInit instanceof Function)
+            onInit(body, mesh, shape);
+          if (!didCreateNewMesh && onUpdate instanceof Function)
+            onUpdate(body, mesh, shape);
+        }
+        meshIndex++;
+      }
+    }
+    for (let i = meshIndex; i < meshes.length; i++) {
+      const mesh = meshes[i];
+      if (mesh) scene.removeMesh(mesh);
+    }
+    meshes.length = meshIndex;
+  }
+
+  return { update };
+} as any;
+
+export default cannonDebugger;
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
 import "@babylonjs/loaders/glTF";
+import CannonDebugger from "./cannon_debugger";
 import {
   Engine,
   Scene,
@@ -14,24 +15,34 @@ import {
   FollowCamera,
   Texture,
   CubeTexture,
-  HavokPlugin,
-  PhysicsShapeSphere,
-  PhysicsShapeBox,
-  Quaternion,
-  PhysicsBody,
-  PhysicsMotionType,
 } from "@babylonjs/core";
-import HavokPhysics from "@babylonjs/havok";
+import {
+  Body,
+  Box,
+  ContactMaterial,
+  Cylinder,
+  Heightfield,
+  Material,
+  Quaternion,
+  RaycastVehicle,
+  Vec3,
+  World,
+} from "cannon-es";
 
 class App {
   readonly #canvas: HTMLCanvasElement;
   readonly #engine: Engine;
   readonly #scene: Scene;
-  inputMap: Record<string, KeyboardEventTypes> = {};
-  car: AbstractMesh | null = null;
-  speed = 50;
-  turnSpeed = 1;
-  camera: FollowCamera | null = null;
+  #physicsDebugger: InstanceType<typeof CannonDebugger> | null = null;
+  #world: World | null = null;
+  readonly #inputMap: Record<string, KeyboardEventTypes> = {};
+  #car: AbstractMesh | null = null;
+  #physicsVehicle: RaycastVehicle | null = null;
+  #camera: FollowCamera | null = null;
+  readonly #groundSize = 1000;
+  readonly #maxSteerVal = 0.5;
+  readonly #maxForce = 200;
+  readonly #brakeForce = 10000;
 
   constructor() {
     this.#canvas = this.#createCanvas();
@@ -41,6 +52,22 @@ class App {
       engine.resize();
     });
     this.#scene = this.#createScene();
+  }
+
+  async setup(): Promise<void> {
+    this.#addObjects();
+    this.#addPhysics();
+    this.#addDebuggers();
+  }
+
+  run(): void {
+    if (this.#world === null) throw new Error("Physics world not initialized");
+
+    this.#engine.runRenderLoop(() => {
+      this.#world?.fixedStep();
+      this.#physicsDebugger?.update();
+      this.#scene.render();
+    });
   }
 
   #createCanvas(): HTMLCanvasElement {
@@ -63,7 +90,7 @@ class App {
     scene.createDefaultSkybox(skyboxTexture, true, 1000);
 
     scene.onKeyboardObservable.add((kbInfo) => {
-      this.inputMap[kbInfo.event.key] = kbInfo.type;
+      this.#inputMap[kbInfo.event.key] = kbInfo.type;
     });
 
     scene.onBeforeRenderObservable.add(() => {
@@ -74,17 +101,17 @@ class App {
   }
 
   #addObjects(): void {
-    this.camera = new FollowCamera(
+    this.#camera = new FollowCamera(
       "Camera",
       new Vector3(0, 10, 0),
       this.#scene,
     );
-    this.camera.radius = 27;
-    this.camera.heightOffset = 10;
-    this.camera.rotationOffset = 180;
-    this.camera.cameraAcceleration = 0.125;
-    this.camera.maxCameraSpeed = 3;
-    this.camera.attachControl(true);
+    this.#camera.radius = 27;
+    this.#camera.heightOffset = 10;
+    this.#camera.rotationOffset = 180;
+    this.#camera.cameraAcceleration = 0.125;
+    this.#camera.maxCameraSpeed = 3;
+    this.#camera.attachControl(true);
 
     SceneLoader.ImportMesh(
       "",
@@ -93,34 +120,14 @@ class App {
       this.#scene,
       (meshes) => {
         const car = meshes[0];
-        this.car = car;
-        this.car.position.y = 1;
-        this.car.rotation = Vector3.Zero();
-        this.car.scaling = new Vector3(0.1, 0.1, 0.1);
+        this.#car = car;
+        this.#car.position.y = 1;
+        this.#car.rotation = Vector3.Zero();
+        this.#car.scaling = new Vector3(0.1, 0.1, 0.1);
 
-        if (this.camera !== null) {
-          this.camera.lockedTarget = this.car;
+        if (this.#camera !== null) {
+          this.#camera.lockedTarget = this.#car;
         }
-
-        const carCenterLocation = this.car
-          .getBoundingInfo()
-          .boundingBox.center.add(new Vector3(2.5, 2, 2));
-        const carPhysicsShape = new PhysicsShapeBox(
-          carCenterLocation,
-          Quaternion.Identity(),
-          new Vector3(7, 6, 15),
-          this.#scene,
-        );
-        const carPhysicsBody = new PhysicsBody(
-          this.car,
-          PhysicsMotionType.DYNAMIC,
-          false,
-          this.#scene,
-        );
-        carPhysicsBody.shape = carPhysicsShape;
-        carPhysicsBody.setMassProperties({
-          mass: 1,
-        });
       },
     );
 
@@ -136,28 +143,12 @@ class App {
         ball.position.z = 40;
         ball.rotation = Vector3.Zero();
         ball.scaling = new Vector3(10, 10, 10);
-
-        const ballPhysicsShape = new PhysicsShapeSphere(
-          ball.getBoundingInfo().boundingSphere.center,
-          4,
-          this.#scene,
-        );
-        const ballPhysicsBody = new PhysicsBody(
-          ball,
-          PhysicsMotionType.DYNAMIC,
-          false,
-          this.#scene,
-        );
-        ballPhysicsBody.shape = ballPhysicsShape;
-        ballPhysicsBody.setMassProperties({
-          mass: 1,
-        });
       },
     );
 
     const ground = MeshBuilder.CreateGround(
       "ground",
-      { width: 1000, height: 1000 },
+      { width: this.#groundSize, height: this.#groundSize },
       this.#scene,
     );
     const groundMaterial = new StandardMaterial("groundMat");
@@ -167,62 +158,224 @@ class App {
     groundMaterial.diffuseTexture = groundTexture;
     ground.material = groundMaterial;
     ground.position.y = 0;
+  }
 
-    const groundPhysicsShape = new PhysicsShapeBox(
-      ground.position,
-      Quaternion.Identity(),
-      new Vector3(1000, 0.1, 1000),
-      this.#scene,
-    );
-    const groundPhysicsBody = new PhysicsBody(
-      ground,
-      PhysicsMotionType.STATIC,
-      false,
-      this.#scene,
-    );
-    groundPhysicsBody.shape = groundPhysicsShape;
-    groundPhysicsBody.setMassProperties({
-      mass: 0,
+  #addPhysics(): void {
+    this.#world = new World();
+    this.#world.defaultContactMaterial.friction = 0;
+    this.#world.gravity.set(0, -10, 0);
+
+    // Build the car chassis
+    const chassisShape = new Box(new Vec3(2, 0.5, 1));
+    const chassisBody = new Body({ mass: 150 });
+    chassisBody.addShape(chassisShape);
+    chassisBody.position.set(0, 4, 0);
+    chassisBody.angularVelocity.set(0, 0.5, 0);
+
+    // Create the vehicle
+    this.#physicsVehicle = new RaycastVehicle({
+      chassisBody,
     });
+
+    const wheelOptions = {
+      radius: 0.5,
+      directionLocal: new Vec3(0, -1, 0),
+      suspensionStiffness: 30,
+      suspensionRestLength: 0.3,
+      frictionSlip: 1.4,
+      dampingRelaxation: 2.3,
+      dampingCompression: 4.4,
+      maxSuspensionForce: 100000,
+      rollInfluence: 0.01,
+      axleLocal: new Vec3(0, 0, 1),
+      chassisConnectionPointLocal: new Vec3(-1, 0, 1),
+      maxSuspensionTravel: 0.3,
+      customSlidingRotationalSpeed: -30,
+      useCustomSlidingRotationalSpeed: true,
+    };
+
+    wheelOptions.chassisConnectionPointLocal.set(-1, 0, 1);
+    this.#physicsVehicle.addWheel(wheelOptions);
+
+    wheelOptions.chassisConnectionPointLocal.set(-1, 0, -1);
+    this.#physicsVehicle.addWheel(wheelOptions);
+
+    wheelOptions.chassisConnectionPointLocal.set(1, 0, 1);
+    this.#physicsVehicle.addWheel(wheelOptions);
+
+    wheelOptions.chassisConnectionPointLocal.set(1, 0, -1);
+    this.#physicsVehicle.addWheel(wheelOptions);
+
+    this.#physicsVehicle.addToWorld(this.#world);
+
+    // Add the wheel bodies
+    const wheelBodies: Body[] = [];
+    const wheelMaterial = new Material("wheel");
+    this.#physicsVehicle.wheelInfos.forEach((wheel) => {
+      const cylinderShape = new Cylinder(
+        wheel.radius,
+        wheel.radius,
+        wheel.radius / 2,
+        20,
+      );
+      const wheelBody = new Body({
+        mass: 0,
+        material: wheelMaterial,
+      });
+      wheelBody.type = Body.KINEMATIC;
+      wheelBody.collisionFilterGroup = 0; // turn off collisions
+      const quaternion = new Quaternion().setFromEuler(-Math.PI / 2, 0, 0);
+      wheelBody.addShape(cylinderShape, new Vec3(), quaternion);
+      wheelBodies.push(wheelBody);
+
+      if (this.#world === null)
+        throw new Error("Physics world failed to initialize");
+
+      this.#world.addBody(wheelBody);
+    });
+
+    // Update the wheel bodies
+    this.#world.addEventListener("postStep", () => {
+      if (this.#physicsVehicle === null)
+        throw new Error("Physics vehicle failed to initialize");
+
+      for (let i = 0; i < this.#physicsVehicle.wheelInfos.length; i++) {
+        this.#physicsVehicle.updateWheelTransform(i);
+        const transform = this.#physicsVehicle.wheelInfos[i].worldTransform;
+        const wheelBody = wheelBodies[i];
+        wheelBody.position.copy(transform.position);
+        wheelBody.quaternion.copy(transform.quaternion);
+      }
+    });
+
+    // Add the ground
+    const matrix: number[][] = [];
+    for (let i = 0; i < this.#groundSize; i++) {
+      matrix.push([]);
+      for (let j = 0; j < this.#groundSize; j++) {
+        const height = 1;
+        matrix[i].push(height);
+      }
+    }
+
+    const groundMaterial = new Material("ground");
+    const heightfieldShape = new Heightfield(matrix, {
+      elementSize: 1,
+    });
+    const heightfieldBody = new Body({
+      mass: 0,
+      material: groundMaterial,
+    });
+    heightfieldBody.addShape(heightfieldShape);
+    heightfieldBody.position.set(
+      -(this.#groundSize * heightfieldShape.elementSize) / 2,
+      -1,
+      (this.#groundSize * heightfieldShape.elementSize) / 2,
+    );
+    heightfieldBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
+    this.#world.addBody(heightfieldBody);
+
+    // Define interactions between wheels and ground
+    const wheelGround = new ContactMaterial(wheelMaterial, groundMaterial, {
+      friction: 0.3,
+      restitution: 0,
+      contactEquationStiffness: 1000,
+    });
+    this.#world.addContactMaterial(wheelGround);
   }
 
   #updateFromKeyboard(): void {
-    if (this.car === null) return;
+    if (this.#physicsVehicle === null) return;
 
-    // Forward and backward movement
-    const newLinearVelocity = Vector3.Zero();
-    const forwardVector = this.car.forward.scale(this.speed);
-
-    if (this.inputMap.w === KeyboardEventTypes.KEYDOWN) {
-      newLinearVelocity.addInPlace(forwardVector);
-    }
-    if (this.inputMap.s === KeyboardEventTypes.KEYDOWN) {
-      newLinearVelocity.subtractInPlace(forwardVector);
-    }
-
-    this.car.physicsBody?.setLinearVelocity(newLinearVelocity);
-
-    // Left and right turning
-    const isMoving = !newLinearVelocity.equals(Vector3.Zero());
-    const isMovingBackward =
-      isMoving && newLinearVelocity.equals(forwardVector.scale(-1));
-    const newAngularVelocity = Vector3.Zero();
-    const turnVector = new Vector3(0, this.turnSpeed, 0);
-    if (isMovingBackward) {
-      turnVector.scaleInPlace(-1);
+    // Accelerating/Reversing
+    if (
+      this.#inputMap.w === KeyboardEventTypes.KEYDOWN &&
+      this.#inputMap.s === KeyboardEventTypes.KEYDOWN
+    ) {
+      this.#physicsVehicle.applyEngineForce(0, 2);
+      this.#physicsVehicle.applyEngineForce(0, 3);
+    } else if (this.#inputMap.w === KeyboardEventTypes.KEYDOWN) {
+      this.#physicsVehicle.applyEngineForce(-this.#maxForce, 2);
+      this.#physicsVehicle.applyEngineForce(-this.#maxForce, 3);
+    } else if (this.#inputMap.s === KeyboardEventTypes.KEYDOWN) {
+      this.#physicsVehicle.applyEngineForce(this.#maxForce, 2);
+      this.#physicsVehicle.applyEngineForce(this.#maxForce, 3);
     }
 
-    if (this.inputMap.a === KeyboardEventTypes.KEYDOWN && isMoving) {
-      newAngularVelocity.subtractInPlace(turnVector);
-    }
-    if (this.inputMap.d === KeyboardEventTypes.KEYDOWN && isMoving) {
-      newAngularVelocity.addInPlace(turnVector);
+    if (this.#inputMap.w === KeyboardEventTypes.KEYUP) {
+      this.#physicsVehicle.applyEngineForce(0, 2);
+      this.#physicsVehicle.applyEngineForce(0, 3);
+
+      // Reset input map so that we do not trigger this code again
+      // Otherwise, we can end up in a state where one key is KEYDOWN and the other is KEYUP
+      delete this.#inputMap.w;
     }
 
-    this.car.physicsBody?.setAngularVelocity(newAngularVelocity);
+    if (this.#inputMap.s === KeyboardEventTypes.KEYUP) {
+      this.#physicsVehicle.applyEngineForce(0, 2);
+      this.#physicsVehicle.applyEngineForce(0, 3);
+
+      // Reset input map so that we do not trigger this code again
+      // Otherwise, we can end up in a state where one key is KEYDOWN and the other is KEYUP
+      delete this.#inputMap.s;
+    }
+
+    // Steering
+    if (
+      this.#inputMap.a === KeyboardEventTypes.KEYDOWN &&
+      this.#inputMap.d === KeyboardEventTypes.KEYDOWN
+    ) {
+      this.#physicsVehicle.setSteeringValue(0, 0);
+      this.#physicsVehicle.setSteeringValue(0, 1);
+    } else if (this.#inputMap.a === KeyboardEventTypes.KEYDOWN) {
+      this.#physicsVehicle.setSteeringValue(-this.#maxSteerVal, 0);
+      this.#physicsVehicle.setSteeringValue(-this.#maxSteerVal, 1);
+    } else if (this.#inputMap.d === KeyboardEventTypes.KEYDOWN) {
+      this.#physicsVehicle.setSteeringValue(this.#maxSteerVal, 0);
+      this.#physicsVehicle.setSteeringValue(this.#maxSteerVal, 1);
+    }
+
+    if (this.#inputMap.a === KeyboardEventTypes.KEYUP) {
+      this.#physicsVehicle.setSteeringValue(0, 0);
+      this.#physicsVehicle.setSteeringValue(0, 1);
+
+      // Reset input map so that we do not trigger this code again
+      // Otherwise, we can end up in a state where one key is KEYDOWN and the other is KEYUP
+      delete this.#inputMap.a;
+    }
+
+    if (this.#inputMap.d === KeyboardEventTypes.KEYUP) {
+      this.#physicsVehicle.setSteeringValue(0, 0);
+      this.#physicsVehicle.setSteeringValue(0, 1);
+
+      // Reset input map so that we do not trigger this code again
+      // Otherwise, we can end up in a state where one key is KEYDOWN and the other is KEYUP
+      delete this.#inputMap.d;
+    }
+
+    // Braking
+    if (this.#inputMap.b === KeyboardEventTypes.KEYDOWN) {
+      this.#physicsVehicle.setBrake(this.#brakeForce, 0);
+      this.#physicsVehicle.setBrake(this.#brakeForce, 1);
+      this.#physicsVehicle.setBrake(this.#brakeForce, 2);
+      this.#physicsVehicle.setBrake(this.#brakeForce, 3);
+    }
+
+    if (this.#inputMap.b === KeyboardEventTypes.KEYUP) {
+      this.#physicsVehicle.setBrake(0, 0);
+      this.#physicsVehicle.setBrake(0, 1);
+      this.#physicsVehicle.setBrake(0, 2);
+      this.#physicsVehicle.setBrake(0, 3);
+
+      delete this.#inputMap.b;
+    }
   }
 
-  #addInspectorListener(): void {
+  #addDebuggers(): void {
+    if (this.#world === null) throw new Error("Physics world not initialized");
+
+    this.#physicsDebugger = new CannonDebugger(this.#scene, this.#world);
+
     window.addEventListener("keydown", (ev) => {
       // Shift+Ctrl+Alt+I
       if (
@@ -237,22 +390,6 @@ class App {
           void this.#scene.debugLayer.show();
         }
       }
-    });
-  }
-
-  async setup(): Promise<void> {
-    const gravityVector = new Vector3(0, -9.81, 0);
-    const havokInstance = await HavokPhysics();
-    const physicsPlugin = new HavokPlugin(true, havokInstance);
-    this.#scene.enablePhysics(gravityVector, physicsPlugin);
-
-    this.#addObjects();
-    this.#addInspectorListener();
-  }
-
-  run(): void {
-    this.#engine.runRenderLoop(() => {
-      this.#scene.render();
     });
   }
 }
